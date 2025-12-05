6c7316e8dfa1ba7e48cfe3f6923060be00d5dda4
MrrClaude <nhemsoksophorn@gamil.com>
2025-12-05 10:59:27 +0700

    first push

---
+++ accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.site_header = "ACLEDA University of Business"
+admin.site.site_title = "ACLEDA University of Business Admin Panel"
+admin.site.index_title = "ACLEDA University of Business Admin Panel"
